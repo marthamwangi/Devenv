09ac5df624cc4c1121a5baef2aaaaa6b7b528b76
Maraqua <martyturing@gmail.com>
2021-08-18 13:22:06 +0300

    final bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,11 +1,21 @@
 /**Creates an Express application. The express() function is a top-level function exported by the express module. */
 import express from "express";
 import path from "path";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 /**Open stuff like URLs, files, executables. Cross-platform. */
 import open from "open";
 const port = 3000;
 /**create an instance of express and set that to app*/
 const app = express();
+/**reference webpack */
+const compiler = webpack(config);
+/**tell express what other things we would like to use */
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 /**tell express which routes it should handle */
 app.get("/", function (req, res) {
   /**Transfer the file at the given path. */
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import numeral from "numeral";
+const courseValue = numeral(1000).format("$0,0.00");
+console.log(`I would like ${courseValue} to pay for the course`);
